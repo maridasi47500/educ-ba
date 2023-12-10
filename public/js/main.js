4f495c882d909d362f950485a298cdda2cfc19c0
theNonDangerousButInvisibleAndAnonymousCoder <thecat@thedog.themouse>
2023-12-10 19:45:35 -0300

    hey

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,20 @@ myCarousel.addEventListener('slide.bs.carousel', event => {
 $("#courselistingtab").children().removeClass("active");
 courselistingtab.children[myitem].toggleClass("active");
 })
+var items=$(courselistingtab).children();
+for(var i=0;i<items.length;i++){
+items[i].dataset.myid=i;
+items[i].children[0].dataset.myid=i;
+$(items[i]).chilren("a").click(function(){
+$(".carousel-indicators button").removeClass("active");
+$(".carousel-item").removeClass("active");
+for(var y=0;y<items.length;y++){
+if (y == $(this)[0].dataset.myid) {
+$($('.carousel-item')[y]).addClass("active");
+$($('.carousel-indicators button')[y]).addClass("active");
+break;
+}
+}
+});
+}
 })
